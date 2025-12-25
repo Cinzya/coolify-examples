0b1d6a880bf748d9494f77a9595e490da33a22ff
Cinzya <cynti-ebert@web.de>
2025-12-25 16:31:51 +0100

    chore: update nuxt static example

---
+++ nuxt/static/nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  ssr: false,
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"],
+    },
+  },
+  runtimeConfig: {
+    public: {
+      appUrl: process.env.APP_URL || "something",
+    },
+  },
+});
